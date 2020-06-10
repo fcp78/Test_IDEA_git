4b544c0c9ef1380d17196555cd1906d627a63ad8
fqh <fqh2@qq.com>
2020-06-10 18:14:04 +0800

    一次修改两个文件

--- src/Hello.java
+++ src/Hello.java
@@ -1,10 +1,8 @@
-/**
- * Created by Admin on 2020/6/10.
- */
 public class Hello {
 
     public static void main(String[] args) {
         System.out.println("asdkljlasdjlkasd");
         System.out.println("sadasklj,,,,,,new modify");
+        //jkjkjkjkjjkjk
     }
 }
