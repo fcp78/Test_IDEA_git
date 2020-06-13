d824428cd27e6c3c10d9142aa357f45618249825
fqh <fqh2@qq.com>
2020-06-13 18:00:12 +0800

    切换到master分支 并进行了修改

--- src/Hello.java
+++ src/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("asdkljlasdjlkasd");
+        //master分支的修改 奥力给
     }
 }
--- src/HelloTest.java
+++ src/HelloTest.java
@@ -1,3 +1,8 @@
+
+/*
+现在试试修改master
+ */
+//再来一行
 public class HelloTest {
 
 }
